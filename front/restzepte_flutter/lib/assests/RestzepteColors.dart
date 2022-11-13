3a075aea3e47ae92bbf5f9fed37f4f2ab797cb68
Janina <101583496+janinaaaa@users.noreply.github.com>
2022-11-13 16:46:23 +0100

    Created all Colors

---
+++ front/restzepte_flutter/lib/assests/RestzepteColors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class RestzepteColors {
+  static const Color background =  Color.fromRGBO(237, 221, 212, 1);
+  static const Color primary =  Color.fromRGBO(63, 99, 95, 1);
+  static const Color secondary =  Color.fromRGBO(170, 103, 95, 1);
+  static const Color highlighted =  Color.fromRGBO(48, 231, 213, 1);
+  static const Color starred = Color.fromRGBO(255, 199, 0, 1);
+  static const Color disabled = Color.fromRGBO(145, 157, 156, 1);
+}
